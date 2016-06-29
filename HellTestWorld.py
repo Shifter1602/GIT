d089be4a002a0f1a4d5d2d356e3373478b9bb685
Whice <and-not@mail.ru>
2016-06-29 15:22:03 +0300

    I am again change file

--- HellTestWorld.py
+++ HellTestWorld.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 import math
+import random
 
 for i in range(1,80):
-    print (math.sqrt(i+i)*i+i)
+    a= ((math.sqrt(i+i)*i+i)*math.sqrt(random.randint(1, 100)))
+    while a>100:
+        a=a-100.1111111
+    print(a)
 
 print("End the program of HellTestWorld")
-=======
-sdgtdeh
-privet kek
->>>>>>> origin/master
